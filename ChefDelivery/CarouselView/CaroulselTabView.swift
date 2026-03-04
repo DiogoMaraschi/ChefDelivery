22a817ae8efb0f427ed2cd1a75234a60ff85d533
DiogoMaraschi <maraschidiogo@gmail.com>
2026-03-04 19:20:29 -0300

    lista de restaurantes adicionada

---
+++ ChefDelivery/CarouselView/CaroulselTabView.swift
@@ -0,0 +1,18 @@
+//
+//  CaroulselTabView.swift
+//  ChefDelivery
+//
+//  Created by Diogo Maraschi on 03/03/26.
+//
+
+import SwiftUI
+
+struct CaroulselTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CaroulselTabView()
+}
---
+++ ChefDelivery/StoresView/StoreItemView.swift
@@ -0,0 +1,18 @@
+//
+//  StoreItemView.swift
+//  ChefDelivery
+//
+//  Created by Diogo Maraschi on 04/03/26.
+//
+
+import SwiftUI
+
+struct StoreItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StoreItemView()
+}
